543af37c289e97b294afb879d7ed1e93532ed618
Joël LAMOTTE <jlamotte@softbankrobotics.com>
2019-09-27 14:52:54 +0200

    Adds basic tests from libsodium makefile

--- tests/basics/driver.c
+++ tests/basics/driver.c
@@ -3,29 +3,12 @@
 #include <string.h>
 #include <assert.h>
 
-#include <libsodium/version.h>
-#include <libsodium/sodium.h>
+#include <sodium/version.h>
+#include <sodium.h>
+
+// TEST: Just checks that compiling and linking works
 
 int main ()
 {
-  char b[256];
-
-  // Basics.
-  //
-  {
-    FILE *o = fmemopen (b, sizeof (b), "w");
-    assert (say_hello (o, "World") > 0);
-    fclose (o);
-    assert (strcmp (b, "Hello, World!\n") == 0);
-  }
-
-  // Empty name.
-  //
-  {
-    FILE *o = fmemopen (b, sizeof (b), "w");
-    assert (say_hello (o, "") < 0 && errno == EINVAL);
-    fclose (o);
-  }
-
   return 0;
 }
